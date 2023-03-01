4d739621439a875fa41002c57506dcd7f727a3ef
novak-ns <105936452+novak-ns@users.noreply.github.com>
2023-03-02 00:42:19 +0100

    not final but close

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,26 @@
+const hamburger = document.getElementById('hamburger');
+const hamburgerspan = document.getElementsByClassName("hamburger_span")
+const navLinks = document.getElementById('nav');
+const body = document.getElementById('body');
+const btnOne = document.getElementById('log-in');
+const btnTwo = document.getElementById('person');
 
-
-      /* $(document).ready(function(){
-
-        $('.icon').on('click', function(e) {
-          e.preventDefault();
-          $(this).closest('.single-item').find('.tooltip-content').addClass("open");
-          console.log("test");
-           $('body').addClass("fixed-class");
-           $('body').addClass("overlay");
-        });
-    
-         $('.tooltip').on('click', function(e) {
-           e.preventDefault();
-           $(this).closest('.single-item').find('.tooltip-content').addClass("open");
-           $('body').addClass("fixed-class");
-           $('body').addClass("overlay");
-         });
-    
-         $('.exit').on('click', function(e) {
-           e.preventDefault();
-           $('.tooltip-content').removeClass("open");
-           $('body').removeClass("fixed-class");
-           $('body').removeClass("overlay");
-         });
-      });  /*
+        
+        document.onclick = function(e){
+            if(e.target.id !== 'nav' && e.target.id !=='hamburger' && e.target.className !=="hamburger_span")
+            {
+                hamburger.classList.remove('open')
+                navLinks.classList.remove('active')
+                body.classList.remove("fixed-class")
+                btnOne.classList.remove('active')
+                btnTwo.classList.remove('active')
+            }
+        }
+        
+        hamburger.onclick = function(){
+            hamburger.classList.toggle('open')
+            navLinks.classList.toggle('active')
+            body.classList.toggle("fixed-class")
+            btnOne.classList.toggle('active')
+            btnTwo.classList.toggle('active')
+        }
